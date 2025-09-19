bb5a6b9e999298438a5b171efc1ba8a633adfc46
wzjadmins <2219718896@qq.com>
2025-09-19 21:50:09 +0800

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,20 @@
+var BaseUrl =  "http://210.16.182.170:1051";
+var ResourceUrl = 'http://210.16.182.170:1999/ttxindasheng/';
+
+if(window.location.pathname!='/'
+    &&window.location.pathname!='/index.html'
+    &&window.location.pathname!='/site/login.html'
+    &&window.location.pathname!='/site/wxredd.html')
+{
+    var wwid=window.localStorage.getItem("wwid");
+    if(!wwid)
+    {
+        localStorage.setItem('enter_url',encodeURIComponent(window.location.href))
+
+        alert('请先登录');
+        window.location.href = '/index.html';
+    }
+}
+
+
+        
